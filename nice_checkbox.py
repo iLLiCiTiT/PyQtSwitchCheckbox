2b741d37f94e4873849fd36e69f9fa9ff258551d
iLLiCiTiT <jakub.trllo@gmail.com>
2020-12-19 14:09:50 +0100

    added `_checked` attribute

--- nice_checkbox.py
+++ nice_checkbox.py
@@ -5,3 +5,12 @@ from Qt import QtWidgets, QtCore, QtGui
 class NiceCheckbox(QtWidgets.QFrame):
     def __init__(self, parent=None):
         super(NiceCheckbox, self).__init__(parent)
+        self._checked = True
+
+        self.checked_color = QtGui.QColor(67, 181, 129)
+        self.unchecked_color = QtGui.QColor(114, 118, 125)
+
+    def setChecked(self, checked):
+        if checked == self._checked:
+            return
+        self._checked = checked
