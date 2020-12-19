4427a16a4970c26735642159ba6a98c7ee787457
iLLiCiTiT <jakub.trllo@gmail.com>
2020-12-19 14:08:52 +0100

    initial commit

---
+++ nice_checkbox.py
@@ -0,0 +1,7 @@
+from math import floor
+from Qt import QtWidgets, QtCore, QtGui
+
+
+class NiceCheckbox(QtWidgets.QFrame):
+    def __init__(self, parent=None):
+        super(NiceCheckbox, self).__init__(parent)
